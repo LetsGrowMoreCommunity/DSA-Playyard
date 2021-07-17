ca1ceb967d136f1da87a983eccb1660841ff7c20
vnhso <vchsiao36@gmail.com>
2021-07-17 11:38:40 -0700

    Update countLeafNodes.py

--- Binary Tree/Python/countLeafNodes.py
+++ Binary Tree/Python/countLeafNodes.py
@@ -1,10 +1,10 @@
-from collections import defaultdict
+#Dummy BT Class
 class bstNode:
 	def __init__(self,val=0,left=None,right=None):
 		self.val = val
 		self.left = left
 		self.right = right
-
+		
 class countLeafNodes:
 	def __init__(self):
 		self.curLeafNodes = 0
