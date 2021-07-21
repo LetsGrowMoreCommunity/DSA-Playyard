b87d20bf486829193f9fe37e1ddf2c358210b8e1
comnk <vhsiao36@gmail.com>
2021-07-20 22:49:33 -0700

    Create 6. Check whether a number is power of four.java

---
+++ Bit-Manipulation/Java/6. Check whether a number is power of four.java
@@ -0,0 +1,54 @@
+/*
+
+6. Check whether a number is power of four(BIT-MANIPULATION)
+
+ This problem is to check if a number is a power of four. It will loop through until (4 ^ i), depending on i,
+ is greater than the given number (return false), or if it equals to the given number (return true)
+
+ Works with numbers up to (2^31 - 1)
+
+ Time Complexity: O(N)
+ Space Complexity O(1)
+ */
+
+public class IfNumberIsPowerOfFour {
+
+    // where method checks if value is power of four
+    public boolean check(long value) {
+        int i = 0;
+
+        // loops until 4 to the power of i is equal to (return true) or greater than value (return false)
+        while (Math.pow(4, i) <= value) {
+            System.out.println(Math.pow(4, i) + " " + value);
+            if (Math.pow(4, i) == value) return true;
+
+            i++;
+        }
+
+        return false;
+    }
+
+    // To use for test cases
+    public void print() {
+
+        /*
+
+        5 -> check(5) -> false
+        64 -> check(64) -> true
+        1073741824 -> check(1073741824) -> true
+
+         */
+
+        System.out.println(check(5));
+        System.out.println(check(16));
+        System.out.println(check(1073741824));
+    }
+
+    // To use for test cases
+    public static void main(String[] args) {
+
+        IfNumberIsPowerOfFour tester = new IfNumberIsPowerOfFour();
+
+        tester.print();
+    }
+}
