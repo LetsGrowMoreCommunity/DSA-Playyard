1cc26617ac3b994bcda77085815bb07e977b179e
sanskritinegi2401@gmail.com <sanskritinegi2401@gmail.com>
2021-07-28 13:28:58 +0530

    Repeated DNA Sequences

---
+++ Hash Table/C++/Repeated DNA Sequences.cpp
@@ -0,0 +1,63 @@
+/*Algorithm: 
+We will store each unique DNA sequence and its frequency in map by traversing from index 0. At last we will return all the
+unique DNA sequence which are repeated by traversing map .
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        
+        unordered_map<string,int>m;
+        vector<string>ans;
+        
+        int n=s.length();
+        
+        for(int i=0;i<=(n-10);i++)
+        {
+            m[s.substr(i,10)]++; //storing string and its frequency of length 10
+        }
+        
+        for(auto x:m) //traversing map
+        {
+            if(x.second>1) //DNA is repeated
+                ans.push_back(x.first); 
+                
+        }
+        
+        return ans;
+        
+    }
+};
+
+
+int main() 
+{
+   
+    string s;
+    cin>>s;
+
+    Solution obj;
+    vector<string>ans=obj.findRepeatedDnaSequences(s);
+
+    for(int i=0;i<ans.size();i++)
+        cout<<ans[i]<<" ";
+       
+    return 0;
+}
+
+// Time Complexity : O(N)
+// Space Complexity : O(1)
+
+/*
+TestCase 1:
+Input: s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+Output: ["AAAAACCCCC","CCCCCAAAAA"]
+
+TestCase 2:
+Input: s = "AAAAAAAAAAAAA"
+Output: ["AAAAAAAAAA"]
+
+*/
