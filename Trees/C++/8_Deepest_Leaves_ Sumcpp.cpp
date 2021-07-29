4e77e44b99190acc29f0f2082a13d36e859885de
Somasree Majumder <56045049+soma2000-lang@users.noreply.github.com>
2021-07-30 04:43:52 +0530

    Add files via upload

---
+++ Trees/C++/8_Deepest_Leaves_ Sumcpp.cpp
@@ -0,0 +1,41 @@
+//Ttime 
+//space complexiity
+//done using bfs
+class Solution { 
+public:
+    int deepestLeavesSum(TreeNode* root) {
+        vector<vector<TreeNode*>> levels;
+        queue<TreeNode*> toDo;//preservr the tree in a queqe since doing bfs
+        toDo.push(root);
+        
+        while(!toDo.empty()){
+            vector<TreeNode*> level;
+            while(!toDo.empty()) { level.push_back(toDo.front()); toDo.pop(); }
+            levels.push_back(level);
+            
+            for(TreeNode* l: level){
+                if(l->left) toDo.push(l->left);
+                if(l->right) toDo.push(l->right);
+            }
+        }
+        
+        int deepestSum(0);
+        for(int i(0); i<levels[levels.size()-1].size(); i++)
+            deepestSum+=(levels[levels.size()-1][i])->val;//addding all the nodes of the last level throuh leve order traversal
+        
+        return deepestSum;//returning the ans
+    }
+};
+int main()
+{
+    vector<int>root;
+    cin>>root;
+    deepestLeavesSum(TreeNode* root)
+}
+/*Input: root = [1,2,3,4,5,null,6,7,null,null,null,null,8]
+Output: 15
+Example 2:
+
+Input: root = [6,7,8,2,7,1,3,9,null,1,4,null,null,null,5]
+Output: 19
+ /*
