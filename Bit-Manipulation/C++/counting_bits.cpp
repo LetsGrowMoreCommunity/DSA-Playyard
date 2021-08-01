d3375e471b53af773634394734a654df44ba3b23
Kanishk Chhabra <67221487+mrkc2303@users.noreply.github.com>
2021-08-01 09:06:37 +0530

    Update counting_bits.cpp

--- Bit-Manipulation/C++/counting_bits.cpp
+++ Bit-Manipulation/C++/counting_bits.cpp
@@ -36,20 +36,28 @@
 
 using namespace std;
 
+//Adding a class of name Solution
 class Solution {
 public:
     vector<int> countBits(int num) {
-        
+        //Defining a vector of type int to store the solution of the problem of size num+1
         vector<int> ans(num + 1);
         for (int i = 0; i <= num; i++) {
             int sum = 0, n = i;
             while (n) {
                 sum++;
                 n &= (n - 1); 
+		// Equivalent to n = n & (n - 1)
+		// & means bitwise AND operator, if we do (1 & 0) we get 0, we get 1 only if both n and (n-1) has 1 on same position
+		// Example: 
+		// Suppose: n =  1100101
+		// Then n & (n-1) will be 1100101 & 1100100 => 1100100
             }
             ans[i] = sum;
+	    // Now here sum calculated will be assigned to ans of index i
         }
         return ans;
+	// We return the ans which stores the solution of our code to the main function
     }
 };
 
@@ -58,9 +66,14 @@ int main() {
 	int n; 
 	cin >> n;
 	Solution s1;
+	// Define an object for the class Solution
+	
 	vector<int> sol;
+	// Calling the function containing our solution using the object: s1
+	// Storinig the returned value in vector sol of type int 
 	sol = s1.countBits(n);
 	
+	// We start printing the solution from here
 	cout << "[";
 	for (int i = 0; i < sol.size(); i++) {
 	    cout << sol[i] << ",";
