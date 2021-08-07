20632e295e91bb1f31622a84d3c46949f8f807dd
Somasree Majumder <56045049+soma2000-lang@users.noreply.github.com>
2021-08-08 04:35:33 +0530

    Update 7_Nth_Digit.cpp

--- Math/C++/7_Nth_Digit.cpp
+++ Math/C++/7_Nth_Digit.cpp
@@ -1,6 +1,10 @@
 
 //Time complexity-0(n)
 //Space Complexity-0(1)
+
+//Algoritm -Explanation-This is a pretty implementation based problem where firsty we are trying to see how many digits the number has,then we are  calculating whuch number is it and
+//and then we are calculating the digit of the number
+
 #include <bits/stdc++.h>
 #include <iostream>
 using namespace std;   
