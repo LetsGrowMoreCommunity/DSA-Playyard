295449aba197fff046bdf0fe10cb19e490db323d
Nisha Yadav <76838660+nisha331@users.noreply.github.com>
2021-08-09 15:41:12 +0530

    required changes

--- Array/Python/count_prime.py
+++ Array/Python/count_prime.py
@@ -1,25 +1,37 @@
- #Count-primes(ARRAYS)
-from math import sqrt
-l=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-n=len(l)
-max_val = l[0];
-for i in range(len(l)):
-    if(l[i]>max_val):
-        max_val = l[i]
-        # USING SIEVE TO FIND ALL PRIME NUMBERS
-prime = [True for i in range(max_val+1)]
-prime[0]=False
-prime[1]=False
-k=int(sqrt(max_val))+1
-for p in range(2, k, 1):
-    if(prime[p]==True):
-        for i in range(p * 2, max_val+1, p):
-            prime[i]=False
-count=0
-for i in range(0, n, 1):
-    if(prime[l[i]]):
-            count+=1
-print(count)
-
-#output 5
+lst = [int(item) for item in input().split()]
+# Function to find count of prime
+def countPrimes(l : [int]) :
+    n=len(l)
+    max_val = l[0];
+    # Find maximum value in the array
+    for i in range(len(l)):
+        if(l[i]>max_val):
+            max_val = l[i]
+        
+    # USING SIEVE TO FIND ALL PRIME NUMBERS
+    prime = [True for i in range(max_val+1)]
+    prime[0]=False
+    prime[1]=False
     
+    k=int(sqrt(max_val))+1
+    for p in range(2, k+1):
+    # If prime[p] is not changed,
+    # then it is a prime
+        if(prime[p]):
+            for i in range(p * p, max_val+1, p):
+                prime[i]=False
+    count=0
+    # Find all primes in arr[]
+    for i in range(0, n, 1):
+        if(prime[l[i]]):
+            count+=1
+    
+    return count
+
+print(countPrimes(lst))
+
+#input 1, 2, 3, 4, 5, 6, 7 
+#output 4
+
+#input 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
+#output 5
